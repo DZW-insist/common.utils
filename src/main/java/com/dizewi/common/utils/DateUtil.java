94ae2f13d222e58f60a24f755003c641c629650a
DZW-insist <984356052@qq.com>
2020-01-03 20:48:17 +0800

    1月3日工具类第一次提交

---
+++ src/main/java/com/dizewi/common/utils/DateUtil.java
@@ -0,0 +1,121 @@
+package com.dizewi.common.utils;
+/** 
+* @author 作者:dizewei
+* @version 创建时间：2020年1月3日 下午3:13:10 
+* 类功能说明 
+*/
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+	public static SimpleDateFormat sdfDate=new SimpleDateFormat("yyyy-MM-dd");
+	public static SimpleDateFormat sdfDateTime=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	
+	
+	/**
+	* @Title: getAge  
+	* @Description:    指定时间计算年龄
+	* @param @param thisDate
+	* @param @return    参数  
+	* @return int    返回类型  
+	* @throws
+	 */
+	public static int getAge(Date thisDate) {
+		
+		//获取当前日期的年月日 
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		//获取生日的日期的年月日
+		calendar.setTime(thisDate);
+		int thisyear = calendar.get(Calendar.YEAR);
+		int thismonth = calendar.get(Calendar.MONTH);
+		int thisday = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		//年龄
+		int age=year-thisyear;
+		//判断月份
+		if(month<thismonth) {
+			age--;
+		}
+		//判断日期
+		if(month==thismonth && day<thisday) {
+			age--;
+		}
+		
+		return age;
+	}
+	
+	public static int getAge(String thisDate) {
+		Date parse;
+		try {
+			parse = sdfDate.parse(thisDate);
+			return getAge(parse);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	/**
+	 * @param date2 
+	* @Title: getDayNum  
+	* @Description: 求两个时间之间的天数  
+	* @param @param date1
+	* @param @param date2
+	* @param @return    参数  
+	* @return int    返回类型  
+	* @throws
+	 */
+	public static int getDayNum(String date1, String date2) {
+		try {
+			Date parse1 = sdfDate.parse(date1);
+			Date parse2 = sdfDate.parse(date2);
+			//一天的毫秒数
+			int dateTime=1000*60*60*24;
+			long time1 = parse1.getTime();
+			long time2 = parse2.getTime();
+			//用数学绝对值计算两时间差
+			 long abs = Math.abs(time1-time2);
+			int i=(int) (abs/dateTime*1);
+			return i;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	/**
+	* @Title: getDayNum  
+	* @Description: 计算未来或者过去 离现在的时间 
+	* @param @param date1
+	* @param @return    参数  
+	* @return int    返回类型  
+	* @throws
+	 */
+	public static int getDayNum(String date1) {
+			Date date = new Date();
+			String date2 = sdfDate.format(date);
+			return getDayNum(date1, date2);
+			
+	}
+	
+	public static boolean inWeek(String date) {
+		
+		return false;
+	}
+	
+	
+	public static void main(String[] args){
+		System.out.println(inWeek("2020-1-9"));
+		
+	}
+	
+}
---
+++ src/main/java/com/dizewi/common/utils/FileUtil.java
@@ -0,0 +1,177 @@
+package com.dizewi.common.utils;
+
+import java.util.Random;
+
+/**
+ * @author 作者:dizewei
+ * @version 创建时间：2020年1月3日 下午3:13:36 类功能说明
+ */
+public class FileUtil {
+
+	
+	/**
+	 * 
+	* @Title: isNull  
+	* @Description: 判断字符串为空，为空时返回true
+	* @param @param str
+	* @param @return    参数  
+	* @return boolean    返回类型  
+	* @throws
+	 */
+	public static boolean isNull(String str) {
+		if (str == null) {
+			return true;
+		}
+		//字符串去除空格
+		str=str.trim();
+		if(str.equals("")) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 
+	* @Title: isNotNull  
+	* @Description: 判断字符串不为空  
+	* @param @param str
+	* @param @return    参数  
+	* @return boolean    返回类型  
+	* @throws
+	 */
+	public static boolean isNotNull(String str) {
+		return !isNull(str);
+	}
+	
+	/**
+	 * 
+	* @Title: inPhone  
+	* @Description: 判断字符串为手机号  
+	* @param @param str
+	* @param @return    参数  
+	* @return boolean    返回类型  
+	* @throws
+	 */
+	public static boolean isPhone(String str) {
+		String regex="1[3578]\\d{9}";	
+		return str.matches(regex);
+	}
+	
+	/**
+	* @Title: isEmail  
+	* @Description: 判断字符串为邮箱
+	* @param @param str
+	* @param @return    参数  
+	* @return boolean    返回类型  
+	* @throws
+	 */
+	public static boolean isEmail(String str) {
+		String regex="\\w+@\\w+(.com|.cn|.com.cn|.net)";	
+		return str.matches(regex);
+	}
+	
+	/**
+	 * 
+	* @Title: isLetter  
+	* @Description:判断字符串为字母
+	* @param @param str
+	* @param @return    参数  
+	* @return boolean    返回类型  
+	* @throws
+	 */
+	public static boolean isLetter(String str) {
+		//字符串不可为空
+		if(isNull(str)) {
+			return false;
+		}
+		//将字符串中的字母转为小写字母
+		str=str.toLowerCase();
+		String regex="[a-z]+";
+		return str.matches(regex);
+	}
+	
+	/**
+	* @Title: getRandomChar  
+	* @Description: 获取随机的英文字母
+	* @param @return    参数  
+	* @return char    返回类型  
+	* @throws
+	 */
+	public static char getRandomLetterChar() {
+		Random random = new Random();
+		char c=(char) ('a'+random.nextInt(26));
+		return c;
+	}
+	
+	/**
+	* @Title: getRandomString  
+	* @Description: 获取随机的指定长度的英文字符串
+	* @param @param i
+	* @param @return    参数  
+	* @return String    返回类型  
+	* @throws
+	 */
+	public static String getRandomLetterString(int num) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < num; i++) {
+			sb.append(getRandomLetterChar());
+		}
+		return sb.toString();
+	}
+	
+	/**
+	* @Title: getRandomNumber  
+	* @Description: 获取随机数字
+	* @param @return    参数  
+	* @return String    返回类型  
+	* @throws
+	 */
+	public static char getRandomNumber() {
+		Random random = new Random();
+		char c=(char) ('0'+random.nextInt(10));
+		return c;
+	}
+	
+	/**
+	* @Title: getRandomNumberString  
+	* @Description:获取随机的指定长度的数字字符串
+	* @param @param num
+	* @param @return    参数  
+	* @return String    返回类型  
+	* @throws
+	 */
+	public static String getRandomNumberString(int num) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i <num; i++) {
+			sb.append(getRandomNumber());
+		}
+		return sb.toString();
+	}
+	
+	
+	/**
+	* @Title: getRandomNumAndChar  
+	* @Description: 获取随机的指定长度的数字字母字符串
+	* @param @param Num
+	* @param @return    参数  
+	* @return String    返回类型  
+	* @throws
+	 */
+	public static String getRandomNumAndChar(int num) {
+		StringBuffer sb = new StringBuffer();
+		Random random = new Random();
+		for (int i = 0; i < num; i++) {
+			int j=random.nextInt(36);
+			//随机数字大于10生成字母
+			if (j>10) {
+			sb.append(getRandomLetterChar());
+			}else{
+				sb.append(getRandomNumber());
+			}
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+				System.out.println(getRandomNumAndChar(6));
+	}	
+}
